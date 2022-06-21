14fd92c9b620008252059f41f619c404671693af
CygnusWall <96161984+CygnusWall@users.noreply.github.com>
2022-06-21 17:45:52 -0400

    Sprite sheet in progress

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -1,10 +1,18 @@
+--allows us to use classes in lua
 Class = require 'lib/class'
 
+--for virtual resolution
 push = require 'lib/push'
 
+--constants that do not change, stored in one file for easy access
 require 'src/constants'
 
+--state machine
 require 'src/StateMachine'
 
+--various states of the game
 require 'src/states/BaseState'
 require 'src/states/StartState'
+
+--utility function used for splitting up the sprite sheet
+require 'src/Util'
